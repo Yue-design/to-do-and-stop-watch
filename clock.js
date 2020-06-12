95e215c53a9daae15a1ec45279b742e4add2c514
Yue-design <65311256+Yue-design@users.noreply.github.com>
2020-06-11 20:52:30 -0400

    Add files via upload

--- clock.js
+++ clock.js
@@ -2,28 +2,49 @@
 clock app
 */
 
-function printFullTime() { // write a function to print the time in 24-hour clock 
-    let currentTime = new Date();
-    let hours = currentTime.getHours();
-    let minutes = currentTime.getMinutes();
-    let seconds = currentTime.getSeconds();
-    document.write(hours + ':' + minutes + ':' + seconds);
-}
-printFullTime();
+// initialization basic variables
+init();
 
-function printHalfTime() { // write a function to print the time in 12-hour clock
-    let currentTime = new Date();
-    let hours = currentTime.getHours();
-    let minutes = currentTime.getMinutes();
-    let seconds = currentTime.getSeconds();
-    if (hours > 12) {
-        document.write(hours + ':' + minutes + ':' + seconds + 'PM');
-    } else {
-        document.write(hours + ':' + minutes + ':' + seconds + 'AM');
-    }
-}
-printHalfTime();
+// add event handlers
+// step one: implementing a twenty-four hour clock to the 'btn-full' button
+document.querySelector('.btn-full').addEventListener('click', () => { 
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    const printTime = hours + ':' + minutes + ':' + seconds;
+    document.querySelector('.clock-0-panel').style.display = 'initial';
+    document.querySelector('.clock-1-panel').style.display = 'none';
+    document.getElementById('time-0').textContent = printTime;
+});
 
+
+// step two: implementing a tweleve-hour clock function to the 'btn-full' button
+document.querySelector('.btn-half').addEventListener('click', () => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    const footer = (hours <= 12) ? 'AM' : 'PM';
+    const printTime2 = hours + ':' + minutes + ':' + seconds + footer;
+    document.querySelector('.clock-1-panel').style.display = 'initial';
+    document.querySelector('.clock-0-panel').style.display = 'none';
+    //document.querySelector('.clock-1-panel').style.display = 'initial';
+    document.getElementById('time-1').textContent = printTime2;
+    // if (hours > 12) {
+    //     document.write(hours + ':' + minutes + ':' + seconds + 'PM');
+    // } else {
+    //     document.write(hours + ':' + minutes + ':' + seconds + 'AM');
+    // }
+});
+
+// step four: connecting with the stop watch
+
+// step three: initialization
+// creating a game initialization function
+document.querySelector('.btn-new').addEventListener('click', init);
+// const init = () => {
 function init() {
-    document.querySelector('.clock').style.display = 'none';
+    document.querySelector('.clock-0-panel').style.display = 'initial';
+    document.querySelector('.clock-1-panel').style.display = 'none';
 }
