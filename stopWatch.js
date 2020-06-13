20e81efe9016c8411495120b3f04f6e2b561d09d
Yue-design <65311256+Yue-design@users.noreply.github.com>
2020-06-13 00:31:29 -0400

    Add files via upload

--- stopWatch.js
+++ stopWatch.js
@@ -1,25 +1,53 @@
-var startTime;
-var stopTime;
+let hour = 0;
+let minute = 0;
+let second = 0;
 
-const watch = () => {
-    const fullDate = new Date();
-    const hour = fullDate.getHours();
-    const minute = fullDate.getMinutes();
-    const second = fullDate.getSeconds();
-    const milliseconds = fullDate.getMilliseconds();
-    const currentTime = (hour, minute, second, milliseconds) => ({hour, minute, second, milliseconds});
-    return currentTime;
-};
+let displayHour, displayMinute, displaySecond;
 
-const displayTime = () => {
-    const startTime = watch();
-    const stopTime = watch();
-    const calcInterval = stopTime - startTime;
-    console.log(calcInterval);
-};
-displayTime();
-// var printInterval = setInterval(() => {
-//     const currentTime = new Date();
-//     const milliseconds = currentTime.getMilliseconds();
-//     document.getElementById('milliseconds').innerHTML = milliseconds;
-//     }, 1000);
+const stopWatch = () => {
+    
+    // increment properly
+    if (second / 60 === 1) {
+        second = 0;
+        minute += 1;   
+        if (minute / 60 === 1) {
+            minute = 0;
+            hour += 1;
+        }
+    }
+
+    // adding a string "0" to the one digit result
+    if (second < 10) {
+        displaySecond = "0" + second.toString();
+    } else {
+        displaySecond = second;
+    }
+
+    if (minute < 10) {
+        displayMinute = "0" + minute.toString();
+    } else {
+        displayMinute = minute;
+    }
+
+    if (hour < 10) {
+        displayHour = "0" + hour.toString();
+    } else {
+        displayHour = hour;
+    }
+
+    console.log(displayHour + ":" + displayMinute + ":" + displaySecond);
+}
+
+window.setInterval(stopWatch, 1000);
+
+// const display = timeValue => {
+//     let displayValue;
+//     if (timeValue < 10) {
+//         displayValue = "0" + timeValue.toString();
+//     } else {
+//         displayValue = timeValue;
+//     }
+//     return displayValue;
+// }
+
+// console.log(display)
